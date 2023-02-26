51956bc016841addf9bacc42d3a5399cb77ba77d
Changhyun <chang.bae@flatironschool.com>
2023-02-26 00:55:09 -0800

    Advanved Functions: First Class Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+}
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(2);
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+const createFareMultiplier = function(x){
+    return function(y){
+        return x*y;
+    }
+}
+const fareDoubler = function(x){
+    return x*2;
+}
+const fareTripler = function(x){
+    return x*3;
+}
+const selectDifferentDrivers = function(drivers,z){
+    return z(drivers);
+}
